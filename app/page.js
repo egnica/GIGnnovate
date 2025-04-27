b097e611cf248a23215dc817ff05f8c9ab03335f
egnica <46502133+egnica@users.noreply.github.com>
2025-04-26 22:56:09 -0500

    front page functions

--- app/page.js
+++ app/page.js
@@ -1,72 +1,135 @@
 "use client";
 import styles from "./page.module.css";
 import { useState, useEffect } from "react";
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
+import GigScroll from "../components/GigScroll.jsx";
 
 export default function Home() {
-  const novateWords = [
-    "Innovate",
-    "Elevate",
-    "Create",
-    "Accelerate",
-    "Activate",
-    "Motivate",
-    "Captivate",
+  const btnTrioText = [
+    {
+      header: "THIS TEXT1",
+      body: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id. Placerat in id cursus mi pretium tellus duis. Pretium tellus duis convallis tempus leo eu aenean.",
+    },
+    {
+      header: "THIS TEXT2",
+      body: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Amet consectetur adipiscing elit quisque faucibus ex sapien. Quisque faucibus ex sapien vitae pellentesque sem placerat. Vitae pellentesque sem placerat in id cursus mi.",
+    },
+    {
+      header: "THIS TEXT3",
+      body: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Adipiscing elit quisque faucibus ex sapien vitae pellentesque. Vitae pellentesque sem placerat in id cursus mi. Cursus mi pretium tellus duis convallis tempus leo. Tempus leo eu aenean sed diam urna tempor. Urna tempor pulvinar vivamus fringilla lacus nec metus.",
+    },
+
+    ,
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [activeSection, setActiveSection] = useState("");
-  const [dynamicWord, setDynamicWord] = useState(novateWords[0]);
+  const [activeSection, setActiveSection] = useState(null);
+  const [isHidden, setIsHidden] = useState(false);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => {
-        const nextIndex = (prevIndex + 1) % novateWords.length;
-        setDynamicWord(novateWords[nextIndex]);
-        return nextIndex;
-      });
-    }, 2000); // Change word every 2 seconds
+  const triClickHandler = (num) => {
+    if (num == activeSection) {
+      setIsHidden(false);
+      setActiveSection(null);
+    } else {
+      setIsHidden(true);
+      setActiveSection(num);
+    }
+  };
 
-    return () => clearInterval(interval);
-  }, []);
+  console.log(isHidden);
+  const colorGrad = () => {
+    return { color: "blue" };
+  };
+
+  const singleBoxVariant = {
+    start: { opacity: 0 },
+    hover: {
+      scale: 1.1,
+      rotate: "2deg",
+      backgroundColor: "rgb(109, 109, 252)",
+    },
+    click: {
+      scale: 0.9,
+      rotate: 0,
+    },
+    animate: {
+      rotateX: 180,
+    },
+  };
 
   return (
     <div className={styles.page}>
-      <div className={styles.heroContain}>
-        <h1>
-          <span style={{ fontSize: "35px", marginTop: "30px" }}>What is</span>
-          <br></br>
-          <span style={{ fontSize: "70px" }}>GIG</span>nnovate?
-        </h1>
-        <hr className={styles.divideLine} />
-        <h3>
-          GIGnnovate helps creators, startups, and small businesses launch
-          modern websites, standout branding, and inspired content. All built
-          with speed, clarity, and intention.
-        </h3>
-        <div className={styles.novateContain}>
-          <h1>GIG</h1>
-          <h1>+</h1>
-          <h1>{dynamicWord}</h1>
-          <h1>=</h1>
+      <div className={styles.headerContain}>
+        <div className={styles.heroOneContain}>
           <h1>
-            <span style={{ fontSize: "50px" }}>GIG</span>nnovate
+            <span style={{ fontSize: "35px", marginTop: "30px" }}>What is</span>
+            <br></br>
+            <span style={{ fontSize: "70px" }}>GIG</span>nnovate?
           </h1>
+          <hr className={styles.divideLine} />
+          <div className={styles.heroDecrip}>
+            <h3>
+              <span style={colorGrad()}>GIGnnovate</span> helps creators,
+              startups, and small businesses launch modern websites, standout
+              branding, and inspired content. All built with speed, clarity, and
+              intention.
+            </h3>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.btnGroup}>
-        <div className={styles.btn} onClick={() => setActiveSection("1")}>
-          What We Do
-        </div>
-        <div className={styles.btn} onClick={() => setActiveSection("2")}>
-          Why GIGnnovate
-        </div>
-        <div className={styles.btn} onClick={() => setActiveSection("3")}>
-          Let’s Work Together
+        <div className={styles.heroTwoContain}>
+          <div className={styles.btnGroup}>
+            <div
+              className={styles.btnHero2}
+              onClick={() => triClickHandler("0")}
+            >
+              What We Do
+            </div>
+            <div
+              className={styles.btnHero2}
+              onClick={() => triClickHandler("1")}
+            >
+              Why GIGnnovate
+            </div>
+            <div
+              className={styles.btnHero2}
+              onClick={() => triClickHandler("2")}
+            >
+              Let’s Work Together
+            </div>
+          </div>
+          <AnimatePresence mode="wait">
+            {isHidden && (
+              <motion.div
+                key={`key to reveal ${isHidden}`}
+                initial={{ height: 0, backgroundColor: "#ffff" }}
+                animate={{ height: "100%", backgroundColor: "#e1e6e6" }}
+                exit={{ height: 0, backgroundColor: "#ffff" }}
+                style={{ overflow: "hidden" }}
+                className={styles.threeButtonReveal}
+              >
+                <AnimatePresence mode="wait">
+                  {activeSection ? (
+                    <motion.div
+                      key={`eachButton: ${activeSection}`}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      className={styles.revealText}
+                    >
+                      <motion.h1 initial={{ x: -100 }} animate={{ x: 0 }}>
+                        {btnTrioText[activeSection].header}
+                      </motion.h1>
+                      <p>{btnTrioText[activeSection].body}</p>
+                    </motion.div>
+                  ) : null}
+                </AnimatePresence>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </div>
-      {}
+      {/* <div style={{ paddingTop: "90px" }}></div> */}
+      <GigScroll />
     </div>
   );
 }
---
+++ components/GigScroll.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "motion/react";
+import styles from "../app/page.module.css";
+
+function GigScroll() {
+  const novateWords = [
+    "Innovate",
+    "Elevate",
+    "Create",
+    "Accelerate",
+    "Activate",
+    "Motivate",
+    "Captivate",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [dynamicWord, setDynamicWord] = useState(novateWords[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => {
+        const nextIndex = (prevIndex + 1) % novateWords.length;
+        setDynamicWord(novateWords[nextIndex]);
+        return nextIndex;
+      });
+    }, 4000); // Change word every X seconds
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className={styles.novateContain}>
+      <h1>GIG</h1>
+      <h1>+</h1>
+      <AnimatePresence mode="wait">
+        <motion.h1
+          key={dynamicWord}
+          initial={{ opacity: 0, y: -30 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 30 }}
+          transition={{ duration: 0.3, ease: "easeIn" }}
+        >
+          {dynamicWord}
+        </motion.h1>
+      </AnimatePresence>
+      <h1>=</h1>
+      <h1>
+        <span style={{ fontSize: "70px" }}>GIG</span>nnovate
+      </h1>
+    </div>
+  );
+}
+
+export default GigScroll;
